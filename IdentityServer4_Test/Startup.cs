8f13eb2fb0aead7e582e893e6d82519ceb957e13
kay Wiberg <kaywib@hotmail.com>
2019-01-13 18:57:47 +0100

    After Adding IdentityServer UI component

--- IdentityServer4_Test/Startup.cs
+++ IdentityServer4_Test/Startup.cs
@@ -19,6 +19,8 @@ namespace IdentityServer4_Test
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            
             services.AddIdentityServer()
             .AddInMemoryClients(Clients.Get())
             .AddInMemoryIdentityResources(IdAndApiResources.GetIdentityResources())
@@ -36,6 +38,9 @@ namespace IdentityServer4_Test
             }
 
             app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
