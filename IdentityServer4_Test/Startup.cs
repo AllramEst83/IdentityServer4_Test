51de7d6386f8cb6e92eda41b70ba41627b59a938
kay Wiberg <kaywib@hotmail.com>
2019-01-13 18:55:14 +0100

    Setting up IdentitySrever4_Befor downloading UI

--- IdentityServer4_Test/Startup.cs
+++ IdentityServer4_Test/Startup.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4_Test.ClientRepo;
+using IdentityServer4_Test.UsersRepo;
+using IdentityServer4_Test.ResourceRepo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +19,12 @@ namespace IdentityServer4_Test
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+            .AddInMemoryClients(Clients.Get())
+            .AddInMemoryIdentityResources(IdAndApiResources.GetIdentityResources())
+            .AddInMemoryApiResources(IdAndApiResources.GetApiResources())
+            .AddTestUsers(Users.Get())
+            .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +35,7 @@ namespace IdentityServer4_Test
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
