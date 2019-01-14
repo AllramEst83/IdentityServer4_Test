174d23d1b9852afd85f09f5661e98bfa4ba4f114
Kay Wiberg <kay.wiberg@gmail.com>
2019-01-14 16:54:06 +0100

    Adding EF to Identity

---
+++ IdentityServer4_Test/Data/Seeder.cs
@@ -0,0 +1,73 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4_Test.ClientRepo;
+using IdentityServer4_Test.ResourceRepo;
+using IdentityServer4_Test.UsersRepo;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer4_Test.Data
+{
+    public static class Seeder
+    {
+        public static void InitializeDbTestData(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
+                //scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
+
+                var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Clients.Get())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in IdAndApiResources.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in IdAndApiResources.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                if (!userManager.Users.Any())
+                {
+                    foreach (var testUser in Users.Get())
+                    {
+                        var identityUser = new IdentityUser(testUser.Username)
+                        {
+                            Id = testUser.SubjectId
+                        };
+
+                        userManager.CreateAsync(identityUser, "Password123!").Wait();
+                        userManager.AddClaimsAsync(identityUser, testUser.Claims.ToList()).Wait();
+                    }
+                }
+            }
+        }
+    }
+}
--- IdentityServer4_Test/Startup.cs
+++ IdentityServer4_Test/Startup.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using IdentityServer4.Models;
-using IdentityServer4_Test.ClientRepo;
-using IdentityServer4_Test.UsersRepo;
-using IdentityServer4_Test.ResourceRepo;
+﻿
+
+using IdentityServer4_Test.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace IdentityServer4_Test
 {
@@ -19,13 +16,50 @@ namespace IdentityServer4_Test
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc();
+
+            const string connectionString = "server=(localdb)\\MSSQLLocalDB;Database=Test.IdentityServer4.EntityFramework;Integrated Security=true;Trusted_Connection=True;MultipleActiveResultSets=true";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+
+
+            services.AddDbContext<ApplicationDbContext>(builder =>
+                builder.UseSqlServer(connectionString, sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services
+                .AddIdentityServer()
+
+            //.AddInMemoryClients(Clients.Get())
+            //.AddInMemoryIdentityResources(IdAndApiResources.GetIdentityResources())
+            //.AddInMemoryApiResources(IdAndApiResources.GetApiResources())
+            //.AddTestUsers(Users.Get())        
+                .AddAspNetIdentity<IdentityUser>()
+
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                })
+
             
-            services.AddIdentityServer()
-            .AddInMemoryClients(Clients.Get())
-            .AddInMemoryIdentityResources(IdAndApiResources.GetIdentityResources())
-            .AddInMemoryApiResources(IdAndApiResources.GetApiResources())
-            .AddTestUsers(Users.Get())
+
             .AddDeveloperSigningCredential();
         }
 
@@ -37,10 +71,14 @@ namespace IdentityServer4_Test
                 app.UseDeveloperExceptionPage();
             }
 
+            Seeder.InitializeDbTestData(app);
+
             app.UseIdentityServer();
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
+
+       
     }
 }
